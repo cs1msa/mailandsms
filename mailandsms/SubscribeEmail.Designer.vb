875f18474d2b2e7d7f7d9903a87978b4f7f8268a
AndreasBagiasTei@gmail.com <AndreasBagiasTei@gmail.com>
2013-01-06 20:26:53 +0200

    ManageSubscribers for Email

---
+++ mailandsms/SubscribeEmail.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SubscribeEmail
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(21, 269)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(569, 212)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label1.Location = New System.Drawing.Point(22, 246)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "All Subscribers"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Button1.Location = New System.Drawing.Point(22, 197)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(223, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "add to list/Subscribe"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Button2.Location = New System.Drawing.Point(407, 489)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(183, 35)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Close"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Enabled = False
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label4.Location = New System.Drawing.Point(28, 532)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Enabled = False
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label5.Location = New System.Drawing.Point(28, 571)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 20)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "email"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Enabled = False
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label6.Location = New System.Drawing.Point(28, 609)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(54, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "phone"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(102, 534)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(102, 573)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(102, 609)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.CheckBox1.Location = New System.Drawing.Point(21, 495)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(202, 24)
+        Me.CheckBox1.TabIndex = 12
+        Me.CheckBox1.Text = "Create New Subscriber"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Button3.Location = New System.Drawing.Point(341, 595)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(168, 36)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Create Subscriber"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Button5.Location = New System.Drawing.Point(229, 489)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(172, 35)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Delete a Subscriber"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(22, 44)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(568, 147)
+        Me.ListBox2.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label3.Location = New System.Drawing.Point(24, 21)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(305, 20)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Current Subscribers that are in the list :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label7.Location = New System.Drawing.Point(423, 21)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(59, 20)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Label7"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Button4.Location = New System.Drawing.Point(260, 197)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(249, 35)
+        Me.Button4.TabIndex = 19
+        Me.Button4.Text = "remove from list/UnSubscribe"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SubscribeEmail
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(620, 661)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Name = "SubscribeEmail"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SubscribeEmail"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+End Class
---
+++ mailandsms/SubscribeEmail.vb
@@ -0,0 +1,275 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Net.Mail
+Public Class SubscribeEmail
+    Dim dbCon As New MySqlConnection("Server=" & My.Settings.mysqlServer & ";Database=" & My.Settings.mysqlDatabase & ";Uid=" & My.Settings.mysqlUsername & ";Pwd=" & My.Settings.mysqlPassword & ";encrypt=" & My.Settings.mysqlSSL)
+    Dim strQuery As String
+    Dim mysqlCmd As MySqlCommand
+    Dim dr As MySqlDataReader
+    Dim thread As System.Threading.Thread
+    
+
+
+    Overridable Sub onload() Handles Me.Load
+        'LIST BOX ME TOUS SUBSCRIBERS OLOUS
+        strQuery = "SELECT * FROM subscriber order by name"
+
+        mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+        dbCon.Open()
+        dr = mysqlCmd.ExecuteReader
+
+        Dim listElements As New ArrayList
+        While dr.Read
+            listElements.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")" & " (" & dr.Item("sms") & ")", dr.Item("id")))
+        End While
+        dr.Close()
+        dbCon.Close()
+        ListBox1.DataSource = listElements
+        ListBox1.DisplayMember = "LongName"
+        ListBox1.ValueMember = "ShortName"
+        'MsgBox(ListBoxLists.SelectedValue.ToString)
+
+        'LIST BOX ME TOUS SUBSCRIBERS TIS CURRENT LISTAS
+        strQuery = "select subscriber.id, subscriber.name, subscriber.email from subscriber, subscribed_to_list " & _
+                            "where subscribed_to_list.subscriber_id=subscriber.id and subscribed_to_list.list_id" & _
+                            "=" & Main.ListBoxLists.SelectedValue.ToString & " order by subscriber.name asc"
+        mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+        dbCon.Open()
+        dr = mysqlCmd.ExecuteReader
+
+        Dim listElements2 As New ArrayList
+        Dim temp_string As String
+        While dr.Read
+            listElements2.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")", dr.Item("id")))
+            temp_string = dr.Item("name")
+        End While
+        dr.Close()
+        dbCon.Close()
+        If temp_string <> "" Then
+            ListBox2.DataSource = listElements2
+            ListBox2.DisplayMember = "LongName"
+            ListBox2.ValueMember = "ShortName"
+        End If
+        'MsgBox(ListBoxLists.SelectedValue.ToString)
+
+
+        '  Label2.Text = Main.ListBoxLists.SelectedItem()
+
+
+
+
+        'ONOMA CURRENT LISTAS STO LABEL
+        strQuery = "SELECT * FROM List where id=" & Main.ListBoxLists.SelectedValue.ToString
+
+        mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+        dbCon.Open()
+        dr = mysqlCmd.ExecuteReader
+        Dim whatlistisit As String
+        '   Dim listElements3 As New ArrayList
+        While dr.Read
+            whatlistisit = dr.Item("name")
+            '     listElements3.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")" & " (" & dr.Item("sms") & ")", dr.Item("id")))
+        End While
+        dr.Close()
+        dbCon.Close()
+        ' ListBox1.DataSource = listElements3
+        '   ListBox1.DisplayMember = "LongName"
+        '   ListBox1.ValueMember = "ShortName"
+        Label7.Text = whatlistisit
+
+
+    End Sub
+
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = False Then
+            Label4.Enabled = False
+            Label5.Enabled = False
+            Label6.Enabled = False
+            TextBox1.Enabled = False
+            TextBox2.Enabled = False
+            TextBox3.Enabled = False
+            Button3.Enabled = False
+
+        Else
+            Label4.Enabled = True
+            Label5.Enabled = True
+            Label6.Enabled = True
+            TextBox1.Enabled = True
+            TextBox2.Enabled = True
+            TextBox3.Enabled = True
+            Button3.Enabled = True
+        End If
+    End Sub
+
+
+
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+
+    Function RandomString(ByVal cb As Integer) As String
+        
+
+    End Function
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+       
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        'BUTTON CREATE SUBSCRIBER
+        Dim idcounter As Integer
+        Try
+            strQuery = "SELECT * FROM Subscriber "
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+            dr = mysqlCmd.ExecuteReader
+            While dr.Read()
+                'vlepo to id tis telefteas seiras tou pinaka Subscriber
+                idcounter = dr.Item("ID")
+            End While
+            dr.Close()
+            dbCon.Close()
+
+            idcounter = idcounter + 1
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+
+        Try
+            strQuery = "insert into Subscriber(id,name,email,sms) values(" & idcounter & ",'" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "')"
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+            dr = mysqlCmd.ExecuteReader
+
+            While dr.Read
+
+            End While
+
+
+            dr.Close()
+            dbCon.Close()
+
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+
+        Try
+            strQuery = "SELECT * FROM subscriber order by name"
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+
+            dr = mysqlCmd.ExecuteReader
+            Dim listElements As New ArrayList
+            While dr.Read
+                listElements.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")" & " (" & dr.Item("sms") & ")", dr.Item("id")))
+            End While
+
+            dr.Close()
+            dbCon.Close()
+
+            ListBox1.DataSource = listElements
+            ListBox1.DisplayMember = "LongName"
+            '  Main.ListBoxLists.ValueMember = "ShortName"
+
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+    End Sub
+
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        'BUTTON DELETE a SUBSCRIBER
+
+        If ListBox1.SelectedIndex <> -1 Then
+            If ListBox1.SelectedValue.GetType.FullName = "System.String" Then
+                strQuery = "delete from Subscriber where id=" & ListBox1.SelectedValue.ToString
+
+                mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+                dbCon.Open()
+                dr = mysqlCmd.ExecuteReader
+
+                Dim listElements As New ArrayList
+                While dr.Read
+                    listElements.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")", dr.Item("id")))
+                End While
+                dr.Close()
+                dbCon.Close()
+                ListBox1.DataSource = listElements
+                ListBox1.DisplayMember = "LongName"
+                ' Main.ListBoxSubscribers.ValueMember = "ShortName"
+                'MsgBox(ListBoxLists.SelectedValue.ToString)
+            End If
+
+            '  MsgBox(ListBox1.SelectedValue.GetType.FullName)
+
+
+        End If
+
+        Try
+            strQuery = "SELECT * FROM subscriber order by name"
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+
+            dr = mysqlCmd.ExecuteReader
+            Dim listElements As New ArrayList
+            While dr.Read
+                listElements.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")" & " (" & dr.Item("sms") & ")", dr.Item("id")))
+            End While
+
+            dr.Close()
+            dbCon.Close()
+
+            ListBox1.DataSource = listElements
+            ListBox1.DisplayMember = "LongName"
+            '  Main.ListBoxLists.ValueMember = "ShortName"
+
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+
+        strQuery = "select subscriber.id, subscriber.name, subscriber.email from subscriber, subscribed_to_list " & _
+                            "where subscribed_to_list.subscriber_id=subscriber.id and subscribed_to_list.list_id" & _
+                            "=" & Main.ListBoxLists.SelectedValue.ToString & " order by subscriber.name asc"
+        mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+        dbCon.Open()
+        dr = mysqlCmd.ExecuteReader
+
+        Dim listElements2 As New ArrayList
+        While dr.Read
+            listElements2.Add(New ListElement(dr.Item("name") & " (" & dr.Item("email") & ")", dr.Item("id")))
+        End While
+        dr.Close()
+        dbCon.Close()
+        ListBox2.DataSource = listElements2
+        ListBox2.DisplayMember = "LongName"
+        ListBox2.ValueMember = "ShortName"
+
+    End Sub
+
+
+    Private Sub Button4_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        
+
+    End Sub
+End Class
