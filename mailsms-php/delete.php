229f16ed87ce1b5be4b0087cb700f75ed98340cf
giorpapantonakis@gmail.com <giorpapantonakis@gmail.com>
2013-01-06 20:45:28 +0200

    Unsubscribe via php page

---
+++ mailsms-php/delete.php
@@ -0,0 +1,41 @@
+﻿<html><head><title>unsubscribe from email sms list</title>
+<link rel="stylesheet" type="text/css" href="mystyles.css"  /></head>
+<body>
+<div class="body">
+<br><br>
+ <CENTER><img src="images/log.jpg">
+<br><br><br>
+<?php
+
+
+require_once("db.php");
+$dbConnection = connectToDB();
+if (isset($_POST['unsubscribe'])){
+$id=$_POST["unsubscribe"];
+$d="http://83.212.99.225/mailsms/index.php?unsubscribe=$id";
+	
+$sql1="SELECT random_string
+		FROM    subscribed_to_list 
+		WHERE random_string ='$d'";
+$result=mysql_query($sql1);
+$b=mysql_num_rows($result);
+if ($b==0)
+{ 
+echo("<P> Δεν υπαρχει στην λιστα</P>");}
+else{
+$a=mysql_result($result,0);
+if( $a!= $d)	
+{ 
+echo("<P>Δεν υπαρχει στην λιστα</P>");}
+else{
+$sql = "Delete from subscribed_to_list where random_string ='$d'";
+if (mysql_query($sql)) 
+    echo("<P> Διαγραφηκε απο την λιστα</P>");
+else 
+    echo("<P>ËÜèïò: " . mysql_error() ."</P>");
+}}}
+
+?>
+</CENTER>
+</bïdy>
+</html>
---
+++ mailsms-php/index.php
@@ -0,0 +1,45 @@
+﻿<html><head><title>unsubscribe from email sms list</title>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<link rel="stylesheet" type="text/css" href="mystyles.css"  /></head>
+<body>
+<br><br>
+ <CENTER><img src="images/log.jpg">
+<br><br><br>
+<?php
+
+
+require_once("db.php");
+$dbConnection = connectToDB();
+if (isset($_GET['unsubscribe'])){
+$id=$_GET["unsubscribe"];
+	
+$d="http://83.212.99.225/mailsms/index.php?unsubscribe=$id";
+	
+$sql="SELECT random_string
+		FROM    subscribed_to_list 
+		WHERE random_string ='$d'";
+
+$result=mysql_query($sql);
+$b=mysql_num_rows($result);
+ 
+if ($b==0)
+{ 
+	echo("<P>  Το unsubscribe link δεν υπαρχει στη λιστα</P>");}
+else{
+
+	
+	echo"<form  method='post' action='delete.php'> ";
+	echo"<P>Θελετε να διαγραφειτε απο τη λιστα αποστολης Email/Sms  ";
+	echo"<input type='submit' value='   Ναι   ' class='button'/> </P>";
+	echo"<input type='hidden' name='unsubscribe' value=\"$id\"/>";
+	echo'</form>';
+ 
+}}
+
+?>
+ </CENTER>
+ 
+</bïdy>
+</html>
+
+
