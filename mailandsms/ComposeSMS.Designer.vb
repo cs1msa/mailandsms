18a30e1b38a9738f92ae144b272d728ef175019f
giorpapantonakis@gmail.com <giorpapantonakis@gmail.com>
2013-01-06 21:09:05 +0200

    Compose sms

---
+++ mailandsms/ComposeSMS.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ComposeSMS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBoxSendLater = New System.Windows.Forms.CheckBox()
+        Me.DateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.ListBoxHour = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBoxMinute = New System.Windows.Forms.ListBox()
+        Me.ToolStripMenuItem8 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem9 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem10 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem11 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem12 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem13 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem14 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem15 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem16 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem17 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem18 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem19 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem20 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem21 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.MenuStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckBoxSendLater
+        '
+        Me.CheckBoxSendLater.AutoSize = True
+        Me.CheckBoxSendLater.Location = New System.Drawing.Point(159, 2)
+        Me.CheckBoxSendLater.Name = "CheckBoxSendLater"
+        Me.CheckBoxSendLater.Size = New System.Drawing.Size(78, 17)
+        Me.CheckBoxSendLater.TabIndex = 12
+        Me.CheckBoxSendLater.Text = "Send Later"
+        Me.CheckBoxSendLater.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker
+        '
+        Me.DateTimePicker.CustomFormat = ""
+        Me.DateTimePicker.Enabled = False
+        Me.DateTimePicker.Location = New System.Drawing.Point(240, 0)
+        Me.DateTimePicker.MinDate = New Date(2012, 12, 4, 0, 0, 0, 0)
+        Me.DateTimePicker.Name = "DateTimePicker"
+        Me.DateTimePicker.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker.TabIndex = 8
+        '
+        'ListBoxHour
+        '
+        Me.ListBoxHour.Enabled = False
+        Me.ListBoxHour.FormattingEnabled = True
+        Me.ListBoxHour.Items.AddRange(New Object() {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"})
+        Me.ListBoxHour.Location = New System.Drawing.Point(444, 2)
+        Me.ListBoxHour.Name = "ListBoxHour"
+        Me.ListBoxHour.ScrollAlwaysVisible = True
+        Me.ListBoxHour.Size = New System.Drawing.Size(36, 17)
+        Me.ListBoxHour.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(482, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(10, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = ":"
+        '
+        'ListBoxMinute
+        '
+        Me.ListBoxMinute.Enabled = False
+        Me.ListBoxMinute.FormattingEnabled = True
+        Me.ListBoxMinute.Items.AddRange(New Object() {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"})
+        Me.ListBoxMinute.Location = New System.Drawing.Point(493, 2)
+        Me.ListBoxMinute.Name = "ListBoxMinute"
+        Me.ListBoxMinute.ScrollAlwaysVisible = True
+        Me.ListBoxMinute.Size = New System.Drawing.Size(36, 17)
+        Me.ListBoxMinute.TabIndex = 11
+        '
+        'ToolStripMenuItem8
+        '
+        Me.ToolStripMenuItem8.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem9, Me.ToolStripSeparator1, Me.ToolStripMenuItem10, Me.ToolStripMenuItem11, Me.ToolStripMenuItem12, Me.ToolStripSeparator2, Me.ToolStripMenuItem13})
+        Me.ToolStripMenuItem8.Name = "ToolStripMenuItem8"
+        Me.ToolStripMenuItem8.Size = New System.Drawing.Size(37, 20)
+        Me.ToolStripMenuItem8.Text = "File"
+        '
+        'ToolStripMenuItem9
+        '
+        Me.ToolStripMenuItem9.Name = "ToolStripMenuItem9"
+        Me.ToolStripMenuItem9.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem9.Text = "Send"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'ToolStripMenuItem10
+        '
+        Me.ToolStripMenuItem10.Name = "ToolStripMenuItem10"
+        Me.ToolStripMenuItem10.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem10.Text = "New"
+        '
+        'ToolStripMenuItem11
+        '
+        Me.ToolStripMenuItem11.Name = "ToolStripMenuItem11"
+        Me.ToolStripMenuItem11.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem11.Text = "Open"
+        '
+        'ToolStripMenuItem12
+        '
+        Me.ToolStripMenuItem12.Name = "ToolStripMenuItem12"
+        Me.ToolStripMenuItem12.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem12.Text = "Save"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(149, 6)
+        '
+        'ToolStripMenuItem13
+        '
+        Me.ToolStripMenuItem13.Name = "ToolStripMenuItem13"
+        Me.ToolStripMenuItem13.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem13.Text = "Close"
+        '
+        'ToolStripMenuItem14
+        '
+        Me.ToolStripMenuItem14.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem15, Me.ToolStripMenuItem16, Me.ToolStripSeparator3, Me.ToolStripMenuItem17, Me.ToolStripMenuItem18, Me.ToolStripMenuItem19, Me.ToolStripSeparator4, Me.ToolStripMenuItem20, Me.ToolStripMenuItem21})
+        Me.ToolStripMenuItem14.Name = "ToolStripMenuItem14"
+        Me.ToolStripMenuItem14.Size = New System.Drawing.Size(39, 20)
+        Me.ToolStripMenuItem14.Text = "Edit"
+        '
+        'ToolStripMenuItem15
+        '
+        Me.ToolStripMenuItem15.Name = "ToolStripMenuItem15"
+        Me.ToolStripMenuItem15.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem15.Text = "Undo"
+        '
+        'ToolStripMenuItem16
+        '
+        Me.ToolStripMenuItem16.Name = "ToolStripMenuItem16"
+        Me.ToolStripMenuItem16.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem16.Text = "Redo"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(119, 6)
+        '
+        'ToolStripMenuItem17
+        '
+        Me.ToolStripMenuItem17.Name = "ToolStripMenuItem17"
+        Me.ToolStripMenuItem17.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem17.Text = "Cut"
+        '
+        'ToolStripMenuItem18
+        '
+        Me.ToolStripMenuItem18.Name = "ToolStripMenuItem18"
+        Me.ToolStripMenuItem18.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem18.Text = "Copy"
+        '
+        'ToolStripMenuItem19
+        '
+        Me.ToolStripMenuItem19.Name = "ToolStripMenuItem19"
+        Me.ToolStripMenuItem19.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem19.Text = "Paste"
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(119, 6)
+        '
+        'ToolStripMenuItem20
+        '
+        Me.ToolStripMenuItem20.Name = "ToolStripMenuItem20"
+        Me.ToolStripMenuItem20.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem20.Text = "Select All"
+        '
+        'ToolStripMenuItem21
+        '
+        Me.ToolStripMenuItem21.Name = "ToolStripMenuItem21"
+        Me.ToolStripMenuItem21.Size = New System.Drawing.Size(122, 22)
+        Me.ToolStripMenuItem21.Text = "Find"
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem8, Me.ToolStripMenuItem14})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(537, 24)
+        Me.MenuStrip2.TabIndex = 7
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.TextBox1.MaxLength = 160
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(537, 63)
+        Me.TextBox1.TabIndex = 13
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 113)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(537, 96)
+        Me.WebBrowser1.TabIndex = 14
+        '
+        'ComposeSMS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 87)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBoxSendLater)
+        Me.Controls.Add(Me.ListBoxMinute)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBoxHour)
+        Me.Controls.Add(Me.DateTimePicker)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "ComposeSMS"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ComposeSMS"
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckBoxSendLater As System.Windows.Forms.CheckBox
+    Friend WithEvents DateTimePicker As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ListBoxHour As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListBoxMinute As System.Windows.Forms.ListBox
+    Friend WithEvents ToolStripMenuItem8 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem9 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem10 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem11 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem12 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem13 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem14 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem15 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem16 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem17 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem18 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem19 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator4 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem20 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem21 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MenuStrip2 As System.Windows.Forms.MenuStrip
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+End Class
---
+++ mailandsms/ComposeSMS.vb
@@ -0,0 +1,115 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Net.Mail
+Public Class ComposeSMS
+    Dim dbCon As New MySqlConnection("Server=" & My.Settings.mysqlServer & ";Database=" & My.Settings.mysqlDatabase & ";Uid=" & My.Settings.mysqlUsername & ";Pwd=" & My.Settings.mysqlPassword & ";encrypt=" & My.Settings.mysqlSSL)
+    Dim strQuery As String
+    Dim mysqlCmd As MySqlCommand
+    Dim dr As MySqlDataReader
+    Dim thread As System.Threading.Thread
+    Private Sub ToolStripMenuItem12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem12.Click
+
+    End Sub
+
+    Private Sub ToolStripMenuItem11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem11.Click
+
+    End Sub
+
+
+    Private Sub ToolStripMenuItem9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem9.Click
+
+        'GET HIGHEST ID of table MESSAGE
+
+        Dim idcounter As Integer
+        Try
+            strQuery = "SELECT * FROM Message "
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+            dr = mysqlCmd.ExecuteReader
+            While dr.Read()
+                'vlepo to id tis telefteas seiras tou pinaka Subscriber
+                idcounter = dr.Item("ID")
+            End While
+            dr.Close()
+            dbCon.Close()
+
+            idcounter = idcounter + 1
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+        'MsgBox(idcounter)
+        'INSERT INTO TABLE MESSAGE
+       Try
+            strQuery = "insert into message(id,subject,message) values(" & idcounter & ",'null','" & TextBox1.Text & "')"
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+            dr = mysqlCmd.ExecuteReader
+
+            While dr.Read
+
+            End While
+
+            dr.Close()
+            dbCon.Close()
+
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+        'CHECK IF TO SEND NOW OR LATER
+        Dim timeString As String
+        If CheckBoxSendLater.Checked Then
+            timeString = Me.DateTimePicker.Value.Year & "-" & Me.DateTimePicker.Value.Month & "-" & Me.DateTimePicker.Value.Day & " " & _
+               Me.ListBoxHour.SelectedItem & ":" & Me.ListBoxMinute.SelectedItem
+        Else
+            timeString = "CURRENT_TIMESTAMP"
+        End If
+        Try
+            strQuery = "insert into listmessage(list_id,message_id,date) values(" & Main.ListBox2.SelectedValue & ",'" & idcounter & "','" & timeString & "')"
+            mysqlCmd = New MySqlCommand(strQuery, dbCon)
+
+            dbCon.Open()
+            dr = mysqlCmd.ExecuteReader
+
+            While dr.Read
+
+            End While
+
+
+            dr.Close()
+            dbCon.Close()
+
+        Catch ex As Exception
+            MsgBox("Failure to communicate with server" & vbCrLf & vbCrLf & ex.Message)
+        End Try
+
+        CheckBoxSendLater.Checked = False
+
+
+        MsgBox("SMS has been sent")
+        Me.Close()
+
+    End Sub
+
+    Private Sub CheckBoxSendLater_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBoxSendLater.CheckedChanged
+        If CheckBoxSendLater.Checked Then
+            DateTimePicker.Enabled = True
+            ListBoxHour.Enabled = True
+            ListBoxMinute.Enabled = True
+        Else
+            DateTimePicker.Enabled = False
+            ListBoxHour.Enabled = False
+            ListBoxMinute.Enabled = False
+            ListBoxHour.SetSelected(0, True)
+            ListBoxMinute.SetSelected(0, True)
+            DateTimePicker.Value = Now
+
+        End If
+    End Sub
+
+
+    Private Sub ComposeSMS_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
